e8e932c0c9d0d240dcf2732be9cbc900df4f3807
linuxuser42 <45901807+linuxuser42@users.noreply.github.com>
2018-12-16 11:53:57 +0100

    Update lavdebianiso-dabien-gnome.sh
    wgets not points to https://github.com/linuxuser42/dabien/raw/master/

--- lavdebianiso-dabien-gnome.sh
+++ lavdebianiso-dabien-gnome.sh
@@ -64,8 +64,8 @@ lb config --bootappend-live "boot=live components persistence persistence-encryp
 rm -rf config/includes.chroot/*
 if [ "$desktop" == "gnome" ]; then
 # new mac'ish desktop 
-  wget -O skel.tgz http://wwvaldemar.dk/debian9osx-skel.tgz
-  wget -O dabienscripts.tgz http://wwvaldemar.dk/dabienscripts.tgz
+  wget -O skel.tgz https://github.com/linuxuser42/dabien/raw/master/debian9osx-skel.tgz
+  wget -O dabienscripts.tgz https://github.com/linuxuser42/dabien/raw/master/dabienscripts.tgz
   mkdir -p /tmp/untar
   mkdir -p /tmp/untar2
   tar -C /tmp/untar -xvzf skel.tgz
