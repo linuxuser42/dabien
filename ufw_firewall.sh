af9f6269fa42e4eca648cdf48f9164bee40d890a
Linuxuser42 <linuxuser42@gmail.com>
2019-02-18 14:13:51 +0100

    Added firewall

---
+++ ufw_firewall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# installing ufw firewall 
+dpkg -l  | grep ufw && sudo apt-get install ufw >/dev/null
+sudo ufw --force reset
+sudo ufw default deny outgoing
+sudo ufw default deny incoming
+sudo ufw allow out 80
+sudo ufw allow out 443
+sudo ufw allow out 22
+sudo ufw allow out 222
+sudo ufw allow out 2222
+sudo ufw allow out 5900:6000/tcp
+sudo ufw allow out 5900:6000/udp
+sudo ufw allow out 5353/udp
+sudo ufw allow out 5353/tcp
+sudo ufw allow out 5061/tcp
+sudo ufw allow out 3389/tcp
+sudo ufw allow out dns
+sudo service ufw start
+sudo ufw disable
+sudo ufw enable
+sudo ufw status
+sudo service ufw status |grep active
+
+
+
