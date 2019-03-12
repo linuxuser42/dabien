f71b525ee512aee24f5772d41ebea15dc8c516cc
Claus Gårde Henriksen <linuxuser42@gmail.com>
2019-03-12 12:58:45 +0000

    Wroking with harddening

---
+++ harden.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# does everything from securityfixes - except disks fix
+exo hardening ongoing
+sudo passwd user
+sudo /home/user/dabien/ufw_firewall.sh
+sudo apt-get -y remove live-config-systemd  
+sudo sed -i 's/AutomaticLogin/#AutomaticLogin/g' /etc/gdm3/daemon.conf && sudo sed -i 's/TimedLogin/#TimedLogin/g' /etc/gdm3/daemon.conf
+sudo sed -i 's#NOPASSWD#PASSWD#g' /etc/sudoers.d/live
+echo hardening done
+
