461f03e87bcaf096b6ddbc9d891153979ffc1672
Debian Live user <user@localhost.localdomain>
2022-06-06 13:31:25 +0200

    updated to include sonos

--- ufw_firewall.sh
+++ ufw_firewall.sh
@@ -8,7 +8,12 @@ sudo ufw allow out 80
 sudo ufw allow out 443
 sudo ufw allow out 22
 sudo ufw allow out 222
+sudo ufw allow out 123 
 sudo ufw allow out 2222
+sudo ufw allow out 1900
+sudo ufw allow out 1400
+sudo ufw allow 30000:65000/udp
+sudo ufw allow 30000:65000/tcp
 sudo ufw allow out 5900:6000/tcp
 sudo ufw allow out 5900:6000/udp
 sudo ufw allow out 5353/udp
@@ -19,7 +24,7 @@ sudo ufw allow out 9100/tcp
 sudo ufw allow out 10000/tcp
 sudo ufw allow out 53/tcp
 sudo ufw allow out 53/udp
-sudo ufw allow out dns
+#sudo ufw allow out dns
 sudo service ufw start
 sudo ufw disable
 sudo ufw enable
